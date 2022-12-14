daf446f2729637cc36148eddbf032358ebc38725
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2022-12-14 10:15:37 +0300

    Start

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'sm': {'max': '375px'}
+    },
+    container: {
+      padding: '20px',
+      center: true,
+    },
+    extend: {
+      colors: {
+        darkBlue: 'hsl(212, 21%, 14%)',
+        grayishBlue: 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)',
+        darkCyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)'
+      }
+    }
+  },
+  plugins: [],
+}
